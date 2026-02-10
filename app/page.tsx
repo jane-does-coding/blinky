28500a32fe65f54e9223ccbec185c1f019fbc218
Jane <yevheniiasimaka@gmail.com>
2026-02-10 15:07:59 -0700

    add transition

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
 	return (
 		<div className="">
-			<img src="/bg3.jpg" className="w-full absolute top-[-20vh]" alt="" />
+			<img src="/image3.png" className="w-full absolute top-[-20vh]" alt="" />
 			<div className="h-[50vh] bg-[#1e2f23]"></div>
 			<div className="flex flex-col items-center justify-center absolute top-[15vh] left-[5vw] bg-red-300/0">
 				<h1 className="wim text-[25vh] text-white leading-[25vh] mx-auto">
@@ -71,16 +71,16 @@ export default function Home() {
 			</div>
 
 			{/*  */}
-			<div className="min-h-[80vh] bg-[#193023] mt-[57.5vh] w-full relative pt-[5vh]">
+			<div className="min-h-[80vh] bg-[#1e2f23] mt-[57.5vh] w-full relative pt-[5vh]">
 				<h1 className="wim text-[20vh] text-white w-fit leading-[20vh] mx-auto ml-[5vw] relative">
 					How it works?
 				</h1>
 				<div className="flex flex-col items-end justify-between mr-[12.5vw] h-[60vh] relative">
 					<div className="flex flex-col h-[60vh] w-[5vw] bg-red-300/0 absolute top-0 right-[-7.5vw] items-center justify-between py-[5vh]">
 						<div className="h-full w-[0.5vw] bg-white absolute top-0 left-[50%] translate-x-[-50%] z-0"></div>
-						<div className="w-[2.5vw] h-[2.5vw] bg-white border-4 border-black z-10"></div>
-						<div className="w-[2.5vw] h-[2.5vw] bg-white border-4 border-black z-10"></div>
-						<div className="w-[2.5vw] h-[2.5vw] bg-white border-4 border-black z-10"></div>
+						<div className="w-[2.5vw] h-[2.5vw] bg-white border-[0.45vh] border-black z-10"></div>
+						<div className="w-[2.5vw] h-[2.5vw] bg-white border-[0.45vh] border-black z-10"></div>
+						<div className="w-[2.5vw] h-[2.5vw] bg-white border-[0.45vh] border-black z-10"></div>
 					</div>
 					<h1 className="wim text-[12.5vh] text-white leading-[15vh] ml-auto">
 						Apply
@@ -96,7 +96,7 @@ export default function Home() {
 				{/* 	<h1 className="wim text-[20vh] text-white leading-[25vh] mx-auto ml-[5vw]">
 					Workshops
 				</h1> */}
-				<div className="flex items-center justify-center gap-[2vh] h-[40vh] bg-red-500/0 min-w-fit group mb-[25vh] mt-[12.5vh]">
+				<div className="flex items-center justify-center gap-[2vh] h-[40vh] bg-red-500/0 min-w-fit group mb-[15vh] mt-[15vh] relative">
 					<img
 						src="/workshop1.png"
 						className="w-[22.5vw] transition-all duration-[0.25s] ease-in-out p-[1vh] bg-white shadow-2xl shadow-black/70 rotate-8 group-hover:rotate-0"
@@ -117,13 +117,44 @@ export default function Home() {
 						className="w-[22.5vw] transition-all duration-[0.25s] ease-in-out p-[1vh] bg-white shadow-2xl shadow-black/70 rotate-[-5deg] group-hover:rotate-0"
 						alt="idk"
 					/>
+					<h1 className="wim text-[10vh] text-white leading-[10vh] ml-auto absolute bottom-[-17vh] left-[5vw]">
+						Workshop Pictures
+					</h1>
 				</div>
-				<div className="h-[80vh] bg-[#20633B] pt-[10vh]">
+				<div className="min-h-[80vh] bg-[#20633B] pb-[15vh]">
+					<img src="another2.png" alt="" className="w-[105vw] left-[-5vw]" />
 					<h1 className="wim text-[20vh] text-white w-fit leading-[20vh] mx-auto ml-[5vw] relative">
 						Blinky Kit Contains
 					</h1>
-					<div className="grid grid-cols-3">
-						<div className="w-full bg-white text-[3vh] wim">Some item</div>
+					<div className="grid grid-cols-3 w-[90vw] mx-auto gap-[2vw] mt-[2vh]">
+						{[
+							"10x Blue LED's",
+							"10x Green LED's",
+							"10x White LED's",
+							"10x Orange LED's",
+							"10x Yellow LED's",
+							"2x 1uF Capacitors",
+							"2x 10nF Capacitors",
+							"2x 1x1 pin headers",
+							"2x 1x2 pin headers",
+							"2x 470 ohm resistors",
+							"2x 1k ohm resistors",
+							"2x 50k variable resistors",
+							"2x 555 Timer IC's",
+							"2x CD4017 LED Controller IC's",
+							"1x 9V Battery Buckle",
+						].map((item, i) => (
+							<div
+								key={i}
+								className="w-full bg-white text-[4vh] wim py-[1vh] px-[2vw] relative border-[0.45vh] border-black/0 font-semibold tracking-[0.15vh]"
+							>
+								<div className="absolute w-[1vh] h-[1vh] bg-[#20633B] top-[-0.5vh] left-[-0.5vh] border-b-[0.45vh] border-r-[0.45vh] border-black/0"></div>
+								<div className="absolute w-[1vh] h-[1vh] bg-[#20633B] top-[-0.5vh] right-[-0.5vh] border-b-[0.45vh] border-l-[0.45vh] border-black/0"></div>
+								<div className="absolute w-[1vh] h-[1vh] bg-[#20633B] bottom-[-0.5vh] left-[-0.5vh] border-t-[0.45vh] border-r-[0.45vh] border-black/0"></div>
+								<div className="absolute w-[1vh] h-[1vh] bg-[#20633B] bottom-[-0.5vh] right-[-0.5vh] border-t-[0.45vh] border-l-[0.45vh] border-black/0"></div>
+								{item}
+							</div>
+						))}
 					</div>
 				</div>
 			</div>
